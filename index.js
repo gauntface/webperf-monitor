10f2505a6b59672389d877022f318a90b68ed529
Matt Gaunt <matt@gauntface.co.uk>
2014-07-03 17:02:53 +0100

    Tidying up some of the logic. Should rip out the threshold to an option

--- index.js
+++ index.js
@@ -16,17 +16,42 @@ function getURLFromUser() {
 	  input: process.stdin,
 	  output: process.stdout
 	});
-
+	var index = 0;
 	rl.question("What is the URL for the sitemap you'd like to crawl? ", function(sitemapUrl) {
 		rl.close();
 
-		pagespeedMonitor.on('onResult', function() {
-			console.log('onResult');
+		pagespeedMonitor.on('onResult', function(url, type, data) {
+			if(data.score < 85) {
+				console.log('---------------------------------');
+				
+				console.log('URL: '+url);
+				console.log('Score: '+data.score);
+
+				var ruleResults = data.formattedResults.ruleResults;
+				console.log('Things to improve:');
+				for(var ruleName in ruleResults) {
+					var ruleResult = ruleResults[ruleName];
+					if(ruleResult.ruleImpact > 0) {
+						console.log('    - '+ruleResult.localizedRuleName);
+						console.log('      '+ruleResult.ruleImpact);
+					}
+				}
+				console.log('---------------------------------');
+				console.log('');
+				console.log('');
+			}
+			index++
 		});
-		
-		pagespeedMonitor.crawlSitemap(sitemapUrl, function() {
+
+		pagespeedMonitor.on('onError', function(err) {
 			getURLFromUser();
 		});
+
+		pagespeedMonitor.on('onCompleted', function() {
+			getURLFromUser();
+		});
+
+		pagespeedMonitor.crawlSitemap(sitemapUrl);
 	});
 }
 
